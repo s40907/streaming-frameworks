5334cebecd85688923cf84018fa7fe2efe067514
Eduard Bergen <bergen.dev@gmail.com>
2014-09-07 22:42:47 +0200

    protopub added feature send messages in 5 minutes

--- prototype/prototypes/protopub/src/main/java/lan/s40907/protopub/App.java
+++ prototype/prototypes/protopub/src/main/java/lan/s40907/protopub/App.java
@@ -4,28 +4,29 @@ import lan.s40907.websocketclient.IWebSocketClient;
 import lan.s40907.websocketclient.WebSocketClientFactory;
 
 import org.json.simple.JSONObject;
-import org.json.simple.JSONArray;
-
 
 public class App 
 {
-
     public static void main( String[] args ) {
-    	 try {   		 
+    	 try {
+    		 IWebSocketClient clientTyrus = WebSocketClientFactory.getInstance();    		 
     		 
-    		 JSONObject json = new JSONObject();
-    		 json.put("remoteType", "backend");
-    		 json.put("sendTime", System.currentTimeMillis());
+    		 long startTime = System.currentTimeMillis();
+    		 while(false||(System.currentTimeMillis()-startTime)<300000)
+    		 {
+    			 JSONObject json = new JSONObject();
+        		 json.put("remoteType", "backend");
+        		 json.put("sendTime", System.currentTimeMillis());
+        		 
+        		 JSONObject wordCount = new JSONObject();
+        		 wordCount.put("word", "protopub");
+        		 wordCount.put("count", 1);
+        		 json.put("wordCount", wordCount);
+    			 clientTyrus.send(json.toJSONString());
+    		 }    		 
     		 
-    		 JSONObject wordCount = new JSONObject();
-    		 wordCount.put("word", "protopub");
-    		 wordCount.put("count", 1);
-    		 json.put("wordCount", wordCount);
-    		 
-    		 IWebSocketClient clientTyrus = WebSocketClientFactory.getInstance();
-    		 clientTyrus.send(json.toJSONString());
     		 clientTyrus.close();
-
+    		 
          } catch (Exception e) {
              e.printStackTrace();
          }
